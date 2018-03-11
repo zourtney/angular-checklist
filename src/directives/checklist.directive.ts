d5cdc6fad60ae5b57f7a402bda1f80c0d82f9b68
Martino Bordin <info@martinobordin.it>
2018-03-11 23:35:27 +0100

    Added maxSelectedItems Input Property
    You can now specify the max number of items a user can select.
    Default is -1 (no limit on selections)

--- src/directives/checklist.directive.ts
+++ src/directives/checklist.directive.ts
@@ -11,6 +11,7 @@ import { Directive, EventEmitter, Input, OnChanges, Output } from '@angular/core
 export class ChecklistDirective implements OnChanges {
   @Input() checklist: any[];
   @Input() checklistValue: any;
+  @Input() maxSelectedItems = -1;
   @Output() checklistChange = new EventEmitter<any[]>();
   isChecked: boolean;
 
@@ -25,13 +26,18 @@ export class ChecklistDirective implements OnChanges {
     const target = $event.target as HTMLInputElement;
     let updatedList;
     const checklist = this.checklist || [];
+
     if (target && target.checked) {
-      updatedList = [...checklist, this.checklistValue];
+      if (this.maxSelectedItems === -1 || checklist.length < this.maxSelectedItems) {
+        updatedList = [...checklist, this.checklistValue];
+        this.checklistChange.emit(updatedList);
+      } else {
+        target.checked = false;
+      }
     } else {
       const i = checklist.indexOf(this.checklistValue);
       updatedList = [...checklist.slice(0, i), ...checklist.slice(i + 1)];
+      this.checklistChange.emit(updatedList);
     }
-
-    this.checklistChange.emit(updatedList);
   }
 }
